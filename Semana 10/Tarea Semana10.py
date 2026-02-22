b44eae86df3afd3fbb5c64d4be4b6ad4836ac8fc
Milton-Paul-Guachala-Quinatoa <mp.guachalaq@uea.edu.ec>
2026-02-21 22:34:27 -0500

    Carga Tarea Semana10

---
+++ Semana 10/Tarea Semana10.py
@@ -0,0 +1,12 @@
+# Matriz 3x3 con números enteros
+
+matriz = [
+    [7, 2, 8],
+    [6, 3, 4],
+    [1, 5, 9]
+]
+
+for i in range(3):
+    for j in range(3):
+        print(matriz[i][j], end=" ")
+    print()
